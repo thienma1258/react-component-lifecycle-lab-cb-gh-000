b2c43d0c4a2088e3a9e7e41aa1d5792dac528d59
Ngoc Dong Pham <cpud1258@gmail.com>
2018-10-03 13:12:12 +0000

    automatically backed up by learn

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -15,7 +15,7 @@ class TweetWall extends React.Component {
     this.setState({tweets:tweets});
   }
 componentWillReceiveProps(nextProps){
-  this.setState({[...this.state.tweets,nextProps.newTweets]});
+  this.setState({tweets:[...this.state.tweets,nextProps.newTweets]});
 
 }
   // TODO: componentWillMount()
